47d0e0e500cc66c3008f128f4a7f3525418615c7
iDereje <d>
2023-07-08 10:11:22 +0300

    added Python Script to auto COMMIT

--- git_work_flow.py
+++ git_work_flow.py
@@ -3,14 +3,17 @@ import datetime
 import os
 
 
-def generate_commit_message():
+def generate_commit_message(custom_message=None):
     # Generate the commit message dynamically based on the current date
-    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-    commit_message = f"Auto commit - {current_date}"
+    if custom_message:
+        commit_message = custom_message
+    else:
+        current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+        commit_message = f"Auto commit - {current_date}"
     return commit_message
 
 
-def git_workflow():
+def git_workflow(commit_message=None):
     # Specify the repository path
     repo_path = "/Users/idereje/Desktop/github/iDereje"
 
@@ -22,12 +25,12 @@ def git_workflow():
         return
 
     # Generate the commit message
-    commit_message = generate_commit_message()
+    final_commit_message = generate_commit_message(commit_message)
 
     # Git commands
     commands = [
         ["git", "add", "."],
-        ["git", "commit", "-m", commit_message],
+        ["git", "commit", "-m", final_commit_message],
         ["git", "push", "origin", "main"],
     ]
 
@@ -38,4 +41,4 @@ def git_workflow():
 
 
 # Call the function to execute the Git workflow
-git_workflow()
+git_workflow("added Python Script to auto COMMIT")
